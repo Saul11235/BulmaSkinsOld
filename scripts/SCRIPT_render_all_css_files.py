dc2cfb8ccc6c6205bc052d3d3ae9f2f38d04250c
Edwin Saul <edwinsaulpm@gmail.com>
2023-12-31 16:31:05 -0500

    add support for autofiles

--- scripts/SCRIPT_render_all_css_files.py
+++ scripts/SCRIPT_render_all_css_files.py
@@ -1,4 +1,33 @@
 # script for render all BulmaSkins css files
 # writed by Edwin Saul
 
+#-------------------------------------------
 
+#-------------------------------------------
+
+#-------------------------------------------
+
+from os.path import isdir, join
+from os import listdir,system
+
+all_name_skins=[]
+all_name_files=listdir("../skinsCode")
+
+for name in all_name_files:
+    if isdir(join("../skinsCode",name)):
+        all_name_skins.append(name)
+
+#-------------------------------------------
+
+def render_template(namestyle):
+    system(f"sass ../skinsCode/{namestyle}/{namestyle}.light.sass ../skins/{namestyle}.css")
+    print("render "+namestyle+" ok!")
+
+#-------------------------------------------
+
+for style in all_name_skins:
+    render_template(style)
+
+#-------------------------------------------
+if __name__=="__main__":
+    input("all css files rendered! ")
