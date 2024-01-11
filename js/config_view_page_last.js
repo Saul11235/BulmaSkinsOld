2b03dc125a2f996f695f8d168c6223bcb1f10d07
Edwin Saul <edwinsaulpm@gmail.com>
2024-01-10 20:27:41 -0500

    generate view on github pages

--- js/config_view_page_last.js
+++ js/config_view_page_last.js
@@ -4,17 +4,40 @@
 function set_text_element(idElement,text) {
   var element=document.getElementById(idElement);
   if (element!==null) {
-    element.innerText=text.trim();
+    element.innerText=text.replace("<","&lt;").replace("\n","").replace(">","&gt;").trim();
   };
 };
-
-
+//---------------------------------------------------------
+function set_href_element(idElement,text) {
+  var element=document.getElementById(idElement);
+  if (element!==null) {
+    element.href=text.replace("<","&lt;").replace(">","&gt;").trim();
+  };
+};
 //---------------------------------------------------------
 
 
 
+set_text_element("title-target",skin+" skin");
+set_text_element("light-header",skin+"-light via CDN");
+set_text_element("light-cdn"   ,'<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Saul11235/BulmaSkins@latest/skins/'+skin+'.light.css">');
+set_text_element("dark-header",skin+"-dark via CDN");
+set_text_element("dark-cdn"   ,'<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Saul11235/BulmaSkins@latest/skins/'+skin+'.dark.css">'); set_text_element("switch-header",skin+" whith switch-color via CDN");
+set_text_element("switch-cdn-1",`<!-- begin BulmaSkins -  `+skin+` -->
+<link id="BulmaSkins-light" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Saul11235/BulmaSkins@latest/skins/`+skins+`.light.css">
+<link id="BulmaSkins-light" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Saul11235/BulmaSkins@latest/skins/`+skins+`.dark.css">
+<script src="https://cdn.jsdelivr.net/gh/Saul11235/BulmaSkins@latest/BulmaSkinsJs/first.js"></script>`); 
+set_text_element("switch-cdn-2",`<button id="BulmaSkins-switch" class="button is-primary"> 
+ <div id="BulmaSkins-switch-light">
+   Light
+ </div>
+ <div id="BulmaSkins-switch-dark" style="display:none">
+   Dark
+ </div>
+</button>`); 
+set_text_element("switch-cdn-3",`<!-- end BulmaSkins -->
+<script src="https://cdn.jsdelivr.net/gh/Saul11235/BulmaSkins@latest/BulmaSkinsJs/last.js"></script>`); 
 
-set_text_element("title_target",skin+" skin");
 
 
 
